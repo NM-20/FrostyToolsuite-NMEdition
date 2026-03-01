04f10ba99daead80d3299578e113df5d91609b46
NM <35472796+NM-20@users.noreply.github.com>
2026-02-28 22:23:26 -0500

    [FrostyEditor & FrostyModManager] Fixed configuration always being overwritten by `Config.UpgradeConfigs`

--- FrostyModManager/App.xaml.cs
+++ FrostyModManager/App.xaml.cs
@@ -109,7 +109,7 @@ namespace FrostyModManager
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            if (!File.Exists($"{Frosty.Core.App.GlobalSettingsPath}/manager_config.json"))
+            if (!File.Exists($"{Frosty.Core.App.GlobalSettingsPath}/Config.json"))
                 Config.UpgradeConfigs();
 
             //RefreshConfigurationList();
